4e86fbc061f92f1fc54f971835b39367fa0b3953
Febrimiji <febrimiji@gmail.com>
2024-12-03 13:15:03 +0800

    Delete 11-12 python directory

--- 11-12 python/12-Latihan/main.py
+++
@@ -1,8 +0,0 @@
-# input dari pengguna
-tinggi = float(input("masukkan tinggi badan : "))
-berat = float(input("masukkan berat badan : "))
-
-tinggi = tinggi / 100
-bmi = berat / (tinggi ** 2 )
-
-print(f"skor BMI adalah : {bmi:.1f}")
