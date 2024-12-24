4272fc8a20c3f5b94d4f5dac643f1b257a2fcbb4
Febrimiji <febrimiji@gmail.com>
2024-12-24 11:43:09 +0800

    Delete MODUL 05 directory

--- MODUL 05/14-LatihanPercabangan/main.py
+++
@@ -1,9 +0,0 @@
-# latihan percabangan
-total_belanja = int(input("Masukkan Total Belanja"))
-
-if total_belanja > 500000:
-    print("Anda mendapatkan Mouse dan keyboard")
-else:
-    print("Belanja Anda Kurang Dari 500rb")
-    print("Anda Mendapatkan Gantungan Kunci")
-print("Terima Kasih")
