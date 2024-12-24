4cd78abc437965144a858f65617d3f171323e2a4
Febrimiji <febrimiji@gmail.com>
2024-12-24 11:38:03 +0800

    Add files via upload

---
+++ MODUL 05/MODUL 05/14-LatihanPercabangan/main.py
@@ -0,0 +1,9 @@
+# latihan percabangan
+total_belanja = int(input("Masukkan Total Belanja"))
+
+if total_belanja > 500000:
+    print("Anda mendapatkan Mouse dan keyboard")
+else:
+    print("Belanja Anda Kurang Dari 500rb")
+    print("Anda Mendapatkan Gantungan Kunci")
+print("Terima Kasih")
